397f4be366e09581b6fa39b86c2630ce09365f85
DESKTOP-F3TFUVN\Ilija Ilic <38-2019@ftn.edu.rs>
2022-01-04 16:15:56 +0100

    Kreirana je baza podataka za AmberAlert aplikaciju, a zatim sam kreirao novi projekat AmberAlert u kojem sam kreirao 3 sloja (presentaion, bussines, data). U data sloju sam kreirao repository klase za svaku tableu u bazi. Kreirana je Shared klasa u kojoj su smešteni interfejsi repository klasa i Models.

---
+++ AmberAlert/Shared/Interfaces/Repository/IFindingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Shared.Interfaces.Repository
+{
+    public interface IFindingRepository
+    {
+        List<Find> GetAllFinding();
+        int InsertFinding(Find f);
+    }
+}
---
+++ AmberAlert/Shared/Models/Find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Find
+    {
+        public DateTime DateOfFinding { get; set; }
+        public string Location { get; set; }
+        public bool Dead { get; set; }
+        public int PoliceId { get; set; }
+        public int IdMis { get; set; }
+
+    }
+}
